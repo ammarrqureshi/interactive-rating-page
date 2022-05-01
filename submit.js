41c90507c076fe32d7688a7bd3f6e1ea2db73d8b
Ammar Qureshi <mamqureshi786@gmail.com>
2022-05-01 16:21:33 +0500

    thank you state appears functionality added, rating changes partial added

--- submit.js
+++ submit.js
@@ -1,4 +1,9 @@
 function submitRating() {
     document.getElementById("main").style.display="none";
     document.getElementById("thank-you").style.display="block";
+
+    
+
+    let selectedRating= document.getElementById("rate-5-number").innerHTML;
+    document.getElementById("selected").innerHTML=selectedRating;
     }
